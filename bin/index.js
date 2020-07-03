4f1c67ce6e7ccea98d280d169cc9efff65fcda6a
HaeJun Seo <kan02134@gmail.com>
2020-07-03 23:50:27 +0900

    Fix option value in optionParser
    change set option without value logic

--- bin/index.js
+++ bin/index.js
@@ -13,7 +13,7 @@ const _ = require('lodash');
  * pilltong --conf ./pill-tong.yml
  */
 const optionParser = _.flowRight(
-  _.partial(_.reduce, _, (r, v) => _.set(r, v[0].replace(/-/g, ''), v[1]), { }),
+  _.partial(_.reduce, _, (r, v) => _.set(r, v[0].replace(/-/g, ''), v[1] || ''), { }),
   _.partial(_.chunk, _, 2),
 );
 
